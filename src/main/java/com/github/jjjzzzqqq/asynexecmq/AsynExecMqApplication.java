6b108b10102b7c96b2f108ae344e73d689afc085
jjjzzzqqq <1791250016@qq.com>
2022-05-27 12:01:37 +0800

    feat:init

---
+++ src/main/java/com/github/jjjzzzqqq/asynexecmq/AsynExecMqApplication.java
@@ -0,0 +1,13 @@
+package com.github.jjjzzzqqq.asynexecmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynExecMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsynExecMqApplication.class, args);
+    }
+
+}
